ec4eb79a990ffb13a78b80a06ce858778fb63790
Ruthsvb <ruthsaraiveb@gmail.com>
2025-07-11 17:03:14 -0400

    README completo, limpieza de requirements.txt y ajustes finales al locustfile

--- locustfile.py
+++ locustfile.py
@@ -1,29 +1,39 @@
 from locust import HttpUser, task, between
-from petstore_api import PetStoreAPI
+import json
 
 
 #se define una clase que simula el comportamiento de un usuario
 class PetstoreUser(HttpUser):
     #se define el tiempo de espera entre las solicitudes
     wait_time = between(1, 3)
+
+    #se define el host (URL base) de la API que se va a probar
+    host = "https://petstore.swagger.io/v2"
     
-   
-    def on_start(self):
-    #Se ejecuatara cuando el usuario virtual "arranca"
-    #Se inicializa la instancia de PetstoreAPI
-        self.api = PetstoreAPI()
 
-        @task
-        def create_pet(self):
-            #Se envia la solicitud para crear una mascot
-            self.api.create_pet("data/create_pet.json")
+    @task
+    def create_pet(self):
+        #Se carga el payload desde el archivo json new_pet.json
+        with open("data/new_pet.json") as f:
+            payload = json.load(f)
 
-        @task   
-        def create_order(self):
-            #Se envia la solicitud para crear un pedido
-            self.api.create_order("data/create_order.json")
+        #Se envia la solicitud POST para crear una mascota
+        self.client.post("/pet",json=payload)
 
-        @task   
-        def create_user(self):
-            #Se envia la solicitud para crear un usuario
-            self.api.create_user("data/create_user.json")
+    @task   
+    def create_order(self):
+        #Se carga el payload desde el archivo json new_order.json
+        with open("data/new_order.json") as f:
+            payload = json.load(f)
+
+        #Se envia la solicitud POST para crear un pedido
+        self.client.post("/store/order", json=payload)
+
+    @task   
+    def create_user(self):      
+        #Se carga el payload desde el archivo json new_user.json
+        with open("data/new_user.json") as f:
+            payload = json.load(f)
+
+        #Se envia la solicitud POST para crear un usuario
+        self.client.post("/user", json=payload)
